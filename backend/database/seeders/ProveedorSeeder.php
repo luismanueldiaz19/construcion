17d49084694fed4c866a613c2414308154ccb573
LuDeveloper (DevSoftLu) <luismanueldiaz19@outlook.es>
2026-06-17 22:31:47 -0400

    seeder modified

--- backend/database/seeders/ProveedorSeeder.php
+++ backend/database/seeders/ProveedorSeeder.php
@@ -25,36 +25,36 @@ class ProveedorSeeder extends Seeder
             'classification' => 'excelente',
         ]);
         
-        Proveedor::create([
-            'code' => 'PROV-0002',
-            'type' => 'empresa',
-            'name' => 'Capiteria Jose',
-            'rnc' => '6253199-5',
-            'phone' => '809-555-5555',
-            'city' => 'Puerto Plata',
-            'classification' => 'bueno',
-        ]);
+        // Proveedor::create([
+        //     'code' => 'PROV-0002',
+        //     'type' => 'empresa',
+        //     'name' => 'Capiteria Jose',
+        //     'rnc' => '6253199-5',
+        //     'phone' => '809-555-5555',
+        //     'city' => 'Puerto Plata',
+        //     'classification' => 'bueno',
+        // ]);
         
-        Proveedor::create([
-            'code' => 'PROV-0003',
-            'type' => 'persona_fisica',
-            'name' => 'Pedro (Albañil)',
-            'rnc' => '8595',
-            'phone' => '809-555-6352',
-            'city' => 'Santiago',
-            'classification' => 'bueno',
-        ]);
+        // Proveedor::create([
+        //     'code' => 'PROV-0003',
+        //     'type' => 'persona_fisica',
+        //     'name' => 'Pedro (Albañil)',
+        //     'rnc' => '8595',
+        //     'phone' => '809-555-6352',
+        //     'city' => 'Santiago',
+        //     'classification' => 'bueno',
+        // ]);
         
-        Proveedor::create([
-            'code' => 'PROV-0004',
-            'type' => 'empresa',
-            'name' => 'IMCA Cat Rentals',
-            'rnc' => '5888859-9',
-            'phone' => '809-560-4622',
-            'address' => 'Autopista Duarte KM 11 ½, Villa Peravia',
-            'city' => 'Santo Domingo',
-            'country' => 'Rep. Dom',
-            'classification' => 'excelente',
-        ]);
+        // Proveedor::create([
+        //     'code' => 'PROV-0004',
+        //     'type' => 'empresa',
+        //     'name' => 'IMCA Cat Rentals',
+        //     'rnc' => '5888859-9',
+        //     'phone' => '809-560-4622',
+        //     'address' => 'Autopista Duarte KM 11 ½, Villa Peravia',
+        //     'city' => 'Santo Domingo',
+        //     'country' => 'Rep. Dom',
+        //     'classification' => 'excelente',
+        // ]);
     }
 }
