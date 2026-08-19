79aebe9c4080505d0517cd06a66106a2fa6f81c2
Lwader Soft <luismanueldiaz19@outlook.es>
2026-08-19 11:37:00 -0400

    cambio de web

--- compilar_web.ps1
+++ compilar_web.ps1
@@ -49,3 +49,8 @@ if (Test-Path $publicIndexHtml) {
 }
 
 Write-Host "¡Proceso terminado con éxito! Listo para hacer git add, commit y push en producción." -ForegroundColor Green
+
+
+
+# powershell -ExecutionPolicy Bypass -File .\compilar_web.ps1
+# toqxih-kAfhyf-xagha6
